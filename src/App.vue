7e92071588a040ae18cb748f0388590a41c07f77
David Hamilton <david@thebanditdave.com>
2018-11-03 11:55:07 -0500

    Fix issues with display on iPhone X

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,12 @@ export default {
 </script>
 
 <style>
+body {
+  padding-top: constant(safe-area-inset-top);
+  padding-top: env(safe-area-inset-top);
+}
+.footer {
+  margin-bottom: constant(safe-area-inset-bottom); /* iOS 11.0 */
+  margin-bottom: env(safe-area-inset-bottom); /* iOS 11.2 */
+}
 </style>
