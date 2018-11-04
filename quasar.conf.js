cdaa8030ae60443c550b5851652b14dfb63f700f
Michael Copanos <michaelcopanos@Michaels-MacBook-Pro.local>
2018-11-04 00:04:28 -0500

    Non-Emergency modal

--- quasar.conf.js
+++ quasar.conf.js
@@ -69,6 +69,9 @@ module.exports = function (ctx) {
         'QDialog',
         'QModal',
         'QModalLayout',
+        'QItemSeparator',
+        'QItemTile',
+        'QRadio',
       ],
       directives: [
         'Ripple',
