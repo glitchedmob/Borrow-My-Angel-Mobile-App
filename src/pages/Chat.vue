94c08fca47db57e4c10d58e542f868b78757c9f4
Levi Zitting <levizitting5663@gmail.com>
2018-11-04 10:25:38 -0600

    Add basic chat ui

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -1,83 +1,108 @@
 <template>
-    <q-layout class="fullscreen">
-      <q-page class="flex flex-center">
-        <q-page-container>
-          <q-modal v-model="moodLevelModal" minimized content-css="padding: 25px">
-            <q-modal-layout>
-              <div class="main column items-center">
-                <q-modal-layout header-style="min-height: 10px">
-                  <h5>HOW ARE YOU FEELING?</h5>
-                </q-modal-layout>
-                <div class="sub column">
-                  <p>1 is the worst</p>
-                  <p>10 is the best</p>
-                </div>
-                <div class="row justify-center">
-                  <template v-for="index in 10" >
-                    <template v-if="index <= moodLevel">
-                      <img
-                              class="q-mx-xs"
-                              :key="index"
-                              src="assets/filledHeart.png"
-                              alt="hearts"
-                              @click="setMoodLevel(index)"
-                      >
-                    </template>
-                    <template v-else>
-                      <img
-                              class="q-mx-xs"
-                              :key="index"
-                              src="assets/emptyHeart.png"
-                              alt="hearts"
-                              @click="setMoodLevel(index)"
-                      >
-                    </template>
-                  </template>
-                </div>
-              </div>
-              <div class=" row justify-center">
-                <q-btn @click="openNextScreen" label="Next" size="md" color="black"/>
-              </div>
-            </q-modal-layout>
-          </q-modal>
-          <q-modal v-model="emergencyModal" minimized content-css="padding: 25px">
-            <q-modal-layout>
-              <div class="main column items-center">
-                <h5>IS YOUR LIFE IN DANGER?</h5>
-                <p>If your life is in danger, <strong>call</strong>
-                  <font color="red"> 911</font> or other emergency responders in your area.</p>
+  <q-layout class="fullscreen">
+    <q-page class="flex wrapper">
+      <q-page-container>
+        <q-modal v-model="moodLevelModal" minimized content-css="padding: 25px">
+          <q-modal-layout>
+            <div class="main column items-center">
+              <q-modal-layout header-style="min-height: 10px">
+                <h5>HOW ARE YOU FEELING?</h5>
+              </q-modal-layout>
+              <div class="sub column">
+                <p>1 is the worst</p>
+                <p>10 is the best</p>
               </div>
               <div class="row justify-center">
-                <q-btn @click="openNonEmergencyModal" label="OK" size="md" color="black"/>
+                <template v-for="index in 10">
+                  <template v-if="index <= moodLevel">
+                    <img
+                      class="q-mx-xs"
+                      :key="index"
+                      src="assets/filledHeart.png"
+                      alt="hearts"
+                      @click="setMoodLevel(index)"
+                    >
+                  </template>
+                  <template v-else>
+                    <img
+                      class="q-mx-xs"
+                      :key="index"
+                      src="assets/emptyHeart.png"
+                      alt="hearts"
+                      @click="setMoodLevel(index)"
+                    >
+                  </template>
+                </template>
               </div>
-            </q-modal-layout>
-          </q-modal>
-          <q-modal v-model="nonEmergencyModal" minimized content-css="padding: 25px">
-            <q-modal-layout>
-              <div class="main row justify-center">
-                <div class="row">
-                  <h5>WHATS GOING ON?</h5>
-                </div>
-                <div class="sub-sub">
-                  <q-item class="column items-start">
-                    <q-radio
-                        v-model="option"
-                        v-for="situation in situations"
-                        :key="situation.id"
-                        :label="situation.title"
-                        :val="situation.value"
-                    />
-                  </q-item>
-                </div>
-                <div class>
-                  <q-btn @click="closeModal" label="OK" size="md" color="black"/>
-                </div>
+            </div>
+            <div class=" row justify-center">
+              <q-btn @click="openNextScreen" label="Next" size="md" color="black" />
+            </div>
+          </q-modal-layout>
+        </q-modal>
+        <q-modal v-model="emergencyModal" minimized content-css="padding: 25px">
+          <q-modal-layout>
+            <div class="main column items-center">
+              <h5>IS YOUR LIFE IN DANGER?</h5>
+              <p>If your life is in danger, <strong>call</strong>
+                <font color="red"> 911</font> or other emergency responders in your area.</p>
+            </div>
+            <div class="row justify-center">
+              <q-btn @click="openNonEmergencyModal" label="OK" size="md" color="black" />
+            </div>
+          </q-modal-layout>
+        </q-modal>
+        <q-modal v-model="nonEmergencyModal" minimized content-css="padding: 25px">
+          <q-modal-layout>
+            <div class="main row justify-center">
+              <div class="row">
+                <h5>WHATS GOING ON?</h5>
               </div>
-            </q-modal-layout>
-          </q-modal>
-        </q-page-container>
-      </q-page>
-    </q-layout>
+              <div class="sub-sub">
+                <q-item class="column items-start">
+                  <q-radio
+                    v-model="option"
+                    v-for="situation in situations"
+                    :key="situation.id"
+                    :label="situation.title"
+                    :val="situation.value"
+                  />
+                </q-item>
+              </div>
+              <div class>
+                <q-btn @click="closeModal" label="OK" size="md" color="black" />
+              </div>
+            </div>
+          </q-modal-layout>
+        </q-modal>
+        <div class="chat-list">
+          <q-chat-message
+            name="Me"
+            :text="['Test Message']"
+            sent
+          />
+          <q-chat-message
+            name="Angel"
+            :text="['Test Message']"
+          />
+        </div>
+        <div class="new-message row fixed-bottom">
+          <q-field class="col-10 new-message-field">
+            <q-input
+              class="q-ma-xs new-message-input"
+              v-model="newMessage"
+              type="textarea"
+              inverted-light
+              color="white"
+            />
+          </q-field>
+          <div class="col-2">
+            <q-btn class="send-button">Send</q-btn>
+          </div>
+        </div>
+      </q-page-container>
+    </q-page>
+  </q-layout>
 </template>
 
 <script>
@@ -87,10 +112,11 @@ export default {
   name: 'PageIndex',
   data() {
     return {
-      moodLevelModal: true,
+      moodLevelModal: false,
       nonEmergencyModal: false,
       emergencyModal: false,
       image: 'assets/emptyHeart.png',
+      newMessage: '',
       situations: [
         {
           id: 1,
@@ -124,6 +150,7 @@ export default {
         },
       ],
       option: '',
+
     };
   },
   computed: {
@@ -156,25 +183,60 @@ export default {
 </script>
 
 <style>
-    body {
-        background-color: #8C9EFF;
-    }
+  body {
+    background-color: #8C9EFF;
+  }
+
+  .wrapper {
+    justify-content: center;
+    align-items: flex-end;
+    padding-bottom: 5rem;
+  }
+
   .logo img {
     width: 100%;
     max-height: 250px;
   }
+
   h5 {
     font-size: 1.2em;
     color: #8C81CC;
   }
+
   .main {
     padding-bottom: 1.5em;
   }
+
   .sub-sub {
     padding-bottom: 2em;
     border: 1px solid transparent
   }
+
   ul {
     list-style-type: none;
+  }
+
+  .chat-list {
+    width: 90vw;
+    margin: 0 auto;
+  }
+
+  .new-message {
+    background: #FFFFFF;
+    width: 100vw;
+    padding: .5rem;
+  }
+
+  .new-message-input {
+    height: 100%;
+    position: relative;
+    top: -4px;
+    border: 1px solid #333;
+  }
+
+  .send-button {
+    background-color: #8C9EFF;
+    color: #FFFFFF;
+    height: 100%;
   }
 </style>
