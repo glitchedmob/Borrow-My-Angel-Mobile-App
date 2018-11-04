027d9fe4c006ab75908d97044509754795dca993
isa-ham <melisadone@yahoo.com>
2018-11-04 01:21:26 -0500

    Moved mood modals to chat page

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -1,13 +1,174 @@
 <template>
   <q-page class="flex flex-center">
+    <q-page-container>
+      <q-modal v-model="moodLevelModal" minimized content-css="padding: 25px">
+        <q-modal-layout>
+          <div class="main column items-center">
+            <q-modal-layout header-style="min-height: 10px">
+              <h5>HOW ARE YOU FEELING?</h5>
+            </q-modal-layout>
+            <div class="sub column">
+              <p>1 is the worst</p>
+              <p>10 is the best</p>
+            </div>
+            <div class="row justify-center">
+              <template v-for="index in 10" >
+                <template v-if="index <= moodLevel">
+                  <img
+                          class="q-mx-xs"
+                          :key="index"
+                          src="assets/filledHeart.png"
+                          alt="hearts"
+                          @click="setMoodLevel(index)"
+                  >
+                </template>
+                <template v-else>
+                  <img
+                          class="q-mx-xs"
+                          :key="index"
+                          src="assets/emptyHeart.png"
+                          alt="hearts"
+                          @click="setMoodLevel(index)"
+                  >
+                </template>
+              </template>
+            </div>
+          </div>
+          <div class=" row justify-center">
+            <q-btn @click="openNextScreen" label="Next" size="md" color="black"/>
+          </div>
+        </q-modal-layout>
+      </q-modal>
+      <q-modal v-model="emergencyModal" minimized content-css="padding: 25px">
+        <q-modal-layout>
+          <div class="main column items-center">
+            <h5>IS YOUR LIFE IN DANGER?</h5>
+            <p>If your life is in danger, <strong>call</strong>
+              <font color="red"> 911</font> or other emergency responders in your area.</p>
+          </div>
+          <div class="row justify-center">
+            <q-btn  @click="openNextScreen" label="OK" size="md" color="black"/>
+          </div>
+        </q-modal-layout>
+      </q-modal>
+      <q-modal v-model="nonEmergencyModal" minimized content-css="padding: 25px">
+        <q-modal-layout>
+          <div class="main row justify-center">
+            <div class="row">
+              <h5>WHATS GOING ON?</h5>
+            </div>
+            <div class="sub-sub">
+              <q-item class="column items-start">
+                <q-radio
+                        v-model="option"
+                        v-for="situation in situations"
+                        :key="situation.id"
+                        :label="situation.title"
+                        :val="situation.value"
+                />
+              </q-item>
+            </div>
+            <div class>
+              <q-btn label="OK" size="md" color="black"/>
+            </div>
+          </div>
+        </q-modal-layout>
+      </q-modal>
+    </q-page-container>
   </q-page>
 </template>
 
-<style>
-</style>
-
 <script>
+import { mapState, mapMutations } from 'vuex';
 
 export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      moodLevelModal: true,
+      nonEmergencyModal: false,
+      emergencyModal: false,
+      image: 'assets/emptyHeart.png',
+      situations: [
+        {
+          id: 1,
+          title: 'I\'m Feeling Sad',
+          value: 'sad',
+        },
+        {
+          id: 2,
+          title: 'I\'m Having A Rough Day',
+          value: 'rough day',
+        },
+        {
+          id: 3,
+          title: 'Mental Abuse',
+          value: 'Mental Abuse',
+        },
+        {
+          id: 4,
+          title: 'Substance Abuse',
+          value: 'Substance Abuse',
+        },
+        {
+          id: 5,
+          title: 'Physical Abuse',
+          value: 'physical Abuse',
+        },
+        {
+          id: 6,
+          title: 'Other',
+          value: 'other',
+        },
+      ],
+      option: '',
+    };
+  },
+  computed: {
+    ...mapState('chat', ['moodLevel']),
+  },
+  methods: {
+    openNextScreen() {
+      if (this.moodLevel >= 6) {
+        this.emergencyModal = !this.emergencyModal;
+        this.openMoodLevelModal();
+      } else {
+        this.nonEmergencyModal = !this.nonEmergencyModal;
+        this.openMoodLevelModal();
+      }
+    },
+    openMoodLevelModal() {
+      this.moodLevelModal = !this.moodLevelModal;
+    },
+    ...mapMutations('chat', ['setMoodLevel']),
+  },
 };
+
 </script>
+
+<style>
+  .background {
+    background-image: url('~assets/bg.png');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  .logo img {
+    width: 100%;
+    max-height: 250px;
+  }
+  h5 {
+    font-size: 1.2em;
+    color: #8C81CC;
+  }
+  .main {
+    padding-bottom: 1.5em;
+  }
+  .sub-sub {
+    padding-bottom: 2em;
+    border: 1px solid transparent
+  }
+  ul {
+    list-style-type: none;
+  }
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -11,83 +11,8 @@
           :to='{ name: "Login" }'
           label="LOGIN"  color="black" class="q-ml-sm my-sm-btn" size="1em"/>
         <br><br>
-        <q-btn label="REQUEST AN ANGEL" @click="openMoodLevelModal" color="red-10" size="1em"/>
+        <q-btn label="REQUEST AN ANGEL" :to='{ name: "Chat" }' color="red-10" size="1em"/>
       </div>
-      <q-page-container>
-        <q-modal v-model="moodLevelModal" minimized content-css="padding: 25px">
-          <q-modal-layout>
-            <div class="main column items-center">
-              <q-modal-layout header-style="min-height: 10px">
-                <h5>HOW ARE YOU FEELING?</h5>
-              </q-modal-layout>
-              <div class="sub column">
-                <p>1 is the worst</p>
-                <p>10 is the best</p>
-              </div>
-              <div class="row justify-center">
-              <template v-for="index in 10" >
-                <template v-if="index <= moodLevel">
-                  <img
-                    class="q-mx-xs"
-                    :key="index"
-                    src="assets/filledHeart.png"
-                    alt="hearts"
-                    @click="setMoodLevel(index)"
-                  >
-                </template>
-                <template v-else>
-                  <img
-                    class="q-mx-xs"
-                    :key="index"
-                    src="assets/emptyHeart.png"
-                    alt="hearts"
-                    @click="setMoodLevel(index)"
-                  >
-                </template>
-              </template>
-              </div>
-            </div>
-            <div class=" row justify-center">
-                <q-btn @click="openNextScreen" label="Next" size="md" color="black"/>
-            </div>
-          </q-modal-layout>
-        </q-modal>
-        <q-modal v-model="emergencyModal" minimized content-css="padding: 25px">
-          <q-modal-layout>
-            <div class="main column items-center">
-              <h5>IS YOUR LIFE IN DANGER?</h5>
-              <p>If your life is in danger, <strong>call</strong>
-              <font color="red"> 911</font> or other emergency responders in your area.</p>
-            </div>
-            <div class="row justify-center">
-              <q-btn label="OK" size="md" color="black"/>
-            </div>
-          </q-modal-layout>
-        </q-modal>
-        <q-modal v-model="nonEmergencyModal" minimized content-css="padding: 25px">
-          <q-modal-layout>
-            <div class="main row justify-center">
-              <div class="row">
-                <h5>WHATS GOING ON?</h5>
-              </div>
-              <div class="sub-sub">
-                <q-item class="column items-start">
-                  <q-radio
-                    v-model="option"
-                    v-for="situation in situations"
-                    :key="situation.id"
-                    :label="situation.title"
-                    :val="situation.value"
-                  />
-                </q-item>
-              </div>
-              <div class>
-                <q-btn label="OK" size="md" color="black"/>
-              </div>
-            </div>
-          </q-modal-layout>
-        </q-modal>
-    </q-page-container>
     </q-page>
     </q-page-container>
   </q-layout>
