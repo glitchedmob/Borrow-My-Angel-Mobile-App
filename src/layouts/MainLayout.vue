35fb38a92487bb60ee026cf3fb6634e572774e47
isa-ham <melisadone@yahoo.com>
2018-11-04 01:36:55 -0600

    Updated modal functionality.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -79,4 +79,7 @@ export default {
 </script>
 
 <style>
+  body {
+    background-color: #8C9EFF;
+  }
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -11,7 +11,8 @@
           :to='{ name: "Login" }'
           label="LOGIN"  color="black" class="q-ml-sm my-sm-btn" size="1em"/>
         <br><br>
-        <q-btn label="REQUEST AN ANGEL" :to='{ name: "Chat" }' color="red-10" size="1em"/>
+        <q-btn :to='{ name: "Chat" }'
+               label="REQUEST AN ANGEL" color="red-10" size="1em"/>
       </div>
     </q-page>
     </q-page-container>
@@ -19,69 +20,8 @@
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex';
-
 export default {
-  name: 'PageIndex',
-  data() {
-    return {
-      moodLevelModal: false,
-      nonEmergencyModal: false,
-      emergencyModal: false,
-      image: 'assets/emptyHeart.png',
-      situations: [
-        {
-          id: 1,
-          title: 'I\'m Feeling Sad',
-          value: 'sad',
-        },
-        {
-          id: 2,
-          title: 'I\'m Having A Rough Day',
-          value: 'rough day',
-        },
-        {
-          id: 3,
-          title: 'Mental Abuse',
-          value: 'Mental Abuse',
-        },
-        {
-          id: 4,
-          title: 'Substance Abuse',
-          value: 'Substance Abuse',
-        },
-        {
-          id: 5,
-          title: 'Physical Abuse',
-          value: 'physical Abuse',
-        },
-        {
-          id: 6,
-          title: 'Other',
-          value: 'other',
-        },
-      ],
-      option: '',
-    };
-  },
-  computed: {
-    ...mapState('chat', ['moodLevel']),
-  },
-  methods: {
-    openNextScreen() {
-      if (this.moodLevel >= 6) {
-        this.emergencyModal = !this.emergencyModal;
-        this.openMoodLevelModal();
-      } else {
-        this.nonEmergencyModal = !this.nonEmergencyModal;
-        this.openMoodLevelModal();
-      }
-    },
-    openMoodLevelModal() {
-      this.moodLevelModal = !this.moodLevelModal;
-    },
-    ...mapMutations('chat', ['setMoodLevel']),
-  },
+
 };
 </script>
 
