1cf4cac35b5007c308d4567d713b1d80cce3c5c0
Michael Copanos <michaelcopanos@Michaels-MBP-010012069215.MissouriState.private>
2018-11-03 20:29:27 -0500

    Emergency modal

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,17 +6,17 @@
     <q-page-container class="">
     <q-page padding class="row justify-center">
       <div class="full-width text-center fixed-bottom q-mb-xl">
-        <q-btn @click="signUp" label="SIGN UP" color="black" class="q-mr-sm my-sm-btn" size="1em"/>
+        <q-btn label="SIGN UP" color="black" class="q-mr-sm my-sm-btn" size="1em"/>
         <q-btn
         :to='{ name: "Login" }'
-        label="LOGIN"  color="black" class="q-ml-sm my-sm-btn" size="1em"/>
+        label="LOGIN" color="black" class="q-ml-sm my-sm-btn" size="1em"/>
         <br><br>
-        <q-btn label="REQUEST AN ANGEL" @click="doReload" color="red-10" size="1em"/>
+        <q-btn label="REQUEST AN ANGEL" @click="openMoodLevelModal" color="red-10" size="1em"/>
       </div>
       <q-page-container>
-        <q-modal class="" v-model="openModal" minimized content-css="padding: 25px">
+        <q-modal v-model="moodLevelModal" minimized content-css="padding: 25px">
           <q-modal-layout>
-            <div class="main row justify-center">
+            <div class="main column items-center">
               <q-modal-layout header-style="min-height: 10px">
                 <h5>HOW ARE YOU FEELING?</h5>
               </q-modal-layout>
@@ -26,13 +26,13 @@
               </div>
               <div class="row justify-center">
               <template v-for="index in 10" >
-                <template v-if="index + 1 <= moodLevel">
+                <template v-if="index <= moodLevel">
                   <img
                     class="q-mx-xs"
                     :key="index"
                     src="assets/filledHeart.png"
                     alt="hearts"
-                    @click="setMoodLevel(index + 1)"
+                    @click="setMoodLevel(index)"
                   >
                 </template>
                 <template v-else>
@@ -41,17 +41,30 @@
                     :key="index"
                     src="assets/emptyHeart.png"
                     alt="hearts"
-                    @click="setMoodLevel(index + 1)"
+                    @click="setMoodLevel(index)"
                   >
                 </template>
               </template>
               </div>
             </div>
             <div class=" row justify-end">
-                <q-btn size="md" color="black">
-                  Next
-                </q-btn>
+                <q-btn @click="openNextScreen" label="Next" size="md" color="black"/>
             </div>
+          </q-modal-layout>
+        </q-modal>
+        <q-modal v-model="emergencyModal" minimized content-css="padding: 25px">
+          <q-modal-layout>
+              <h5>IS YOUR LIFE IN DANGER?</h5>
+              <p>If your life is in danger, <strong>call</strong>
+              <font color="red"> 911</font> or other emergency responders in your area.</p>
+              <div class="row justify-center">
+                <q-btn @click="openNextScreen" label="Next" size="md" color="black"/>
+              </div>
+          </q-modal-layout>
+        </q-modal>
+        <q-modal v-model="nonEmergencyModal" minimized content-css="padding: 25px">
+          <q-modal-layout>
+            <h5>WHATS GOING ON?</h5>
           </q-modal-layout>
         </q-modal>
     </q-page-container>
@@ -59,6 +72,40 @@
     </q-page-container>
   </q-layout>
 </template>
+
+<script>
+import { mapState, mapMutations } from 'vuex';
+
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      moodLevelModal: false,
+      nonEmergencyModal: false,
+      emergencyModal: false,
+      image: 'assets/emptyHeart.png',
+    };
+  },
+  computed: {
+    ...mapState('chat', ['moodLevel']),
+  },
+  methods: {
+    openNextScreen() {
+      if (this.moodLevel >= 6) {
+        this.emergencyModal = !this.emergencyModal;
+        this.openMoodLevelModal();
+      } else {
+        this.nonEmergencyModal = !this.nonEmergencyModal;
+        this.openMoodLevelModal();
+      }
+    },
+    openMoodLevelModal() {
+      this.moodLevelModal = !this.moodLevelModal;
+    },
+    ...mapMutations('chat', ['setMoodLevel']),
+  },
+};
+</script>
 
 <style>
   .background {
@@ -82,30 +129,3 @@
     padding-bottom: 2em;
   }
 </style>
-
-<script>
-import { mapState, mapMutations } from 'vuex';
-
-export default {
-  name: 'PageIndex',
-  data() {
-    return {
-      openModal: true,
-      image: 'assets/emptyHeart.png',
-    };
-  },
-  computed: {
-    ...mapState('chat', ['moodLevel']),
-  },
-  methods: {
-    doOpenModal() {
-      this.openModal = !this.openModal;
-    },
-    doReload() {
-      window.location.reload();
-      this.doOpenModal();
-    },
-    ...mapMutations('chat', ['setMoodLevel']),
-  },
-};
-</script>
